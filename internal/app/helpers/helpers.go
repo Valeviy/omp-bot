dfdca6c7f07f7b64c0127fd9458073e1f103e434
vyurinskaya <vyurinskaya@corcentric.com>
2021-10-21 16:37:52 +0300

    [Task-1] Use time.Time for CreatedAt and DoneAt and EquipmentRequestStatus for Status

--- internal/app/helpers/helpers.go
+++ internal/app/helpers/helpers.go
@@ -1,20 +1,5 @@
 package helpers
 
-import (
-	"log"
-	"time"
-)
-
-func DateFormatter(date string, fromLayout string, toLayout string) string {
-	parsedDate, err := time.Parse(fromLayout, date)
-	if err != nil {
-		log.Printf("fail to parse date %s: %v", date, err)
-		return date
-	}
-
-	return parsedDate.Format(toLayout)
-}
-
 func Min(x, y uint64) uint64 {
 	if x > y {
 		return y
--- internal/model/business/models.go
+++ internal/model/business/models.go
@@ -2,22 +2,21 @@ package business
 
 import (
 	"fmt"
-	"github.com/ozonmp/omp-bot/internal/app/helpers"
+	"time"
 )
 
 const (
-	dataBaseDateFormat = "2006-01-02T15:04:05"
-	outputDateFormat   = "2 Jan 2006 15:04:05"
+	outputDateFormat = "2 Jan 2006 15:04:05"
 )
 
 type EquipmentRequest struct {
-	Id            uint64 `json:"-"`
-	EmployeeId    uint64 `json:"employee_id"`
-	EquipmentType string `json:"equipment_type"`
-	EquipmentId   uint64 `json:"equipment_id"`
-	CreatedAt     string `json:"created_at"`
-	DoneAt        string `json:"done_at"`
-	Status        bool   `json:"status"`
+	Id            uint64                 `json:"-"`
+	EmployeeId    uint64                 `json:"employee_id"`
+	EquipmentType string                 `json:"equipment_type"`
+	EquipmentId   uint64                 `json:"equipment_id"`
+	CreatedAt     time.Time              `json:"created_at"`
+	DoneAt        time.Time              `json:"done_at"`
+	Status        EquipmentRequestStatus `json:"status"`
 }
 
 type EquipmentRequestList struct {
@@ -25,29 +24,39 @@ type EquipmentRequestList struct {
 	List   []EquipmentRequest
 }
 
-func (equipmentRequest *EquipmentRequest) String() string {
-	createdAtDate := helpers.DateFormatter(equipmentRequest.CreatedAt, dataBaseDateFormat, outputDateFormat)
-	doneAtDate := helpers.DateFormatter(equipmentRequest.DoneAt, dataBaseDateFormat, outputDateFormat)
+type EquipmentRequestStatus int
 
-	return fmt.Sprintf("EquipmentRequest ID: %d,\nEmployee ID: %d,\nEquipment Type: %s,\nEquipment ID: %d,\nCreated at: %s,\nDone at: %s,\nStatus: %t \n",
-		equipmentRequest.Id,
-		equipmentRequest.EmployeeId,
-		equipmentRequest.EquipmentType,
-		equipmentRequest.EquipmentId,
-		createdAtDate,
-		doneAtDate,
-		equipmentRequest.Status,
+const (
+	Do EquipmentRequestStatus = iota
+	InProgress
+	Done
+	Cancelled
+)
+
+func (es EquipmentRequestStatus) String() string {
+	return [...]string{"Do", "In Progress", "Done", "Cancelled"}[es]
+}
+
+func (e *EquipmentRequest) String() string {
+	return fmt.Sprintf("EquipmentRequest ID: %d,\nEmployee ID: %d,\nEquipment Type: %s,\nEquipment ID: %d,\nCreated at: %s,\nDone at: %s,\nStatus: %s \n",
+		e.Id,
+		e.EmployeeId,
+		e.EquipmentType,
+		e.EquipmentId,
+		e.CreatedAt.Format(outputDateFormat),
+		e.DoneAt.Format(outputDateFormat),
+		e.Status,
 	)
 }
 
 var AllEquipmentRequests = EquipmentRequestList{
 	6,
 	[]EquipmentRequest{
-		{Id: 1, EmployeeId: 1, EquipmentType: "Laptop", EquipmentId: 1, CreatedAt: "2020-01-19T10:00:00", DoneAt: "2020-01-19T10:00:00", Status: true},
-		{Id: 2, EmployeeId: 1, EquipmentType: "Keyboard", EquipmentId: 1, CreatedAt: "2020-01-19T10:00:00", DoneAt: "2020-01-19T10:00:00", Status: true},
-		{Id: 3, EmployeeId: 1, EquipmentType: "Keyboard", EquipmentId: 1, CreatedAt: "2020-01-19T10:00:00", DoneAt: "2020-01-19T10:00:00", Status: true},
-		{Id: 4, EmployeeId: 2, EquipmentType: "Laptop", EquipmentId: 2, CreatedAt: "2020-01-19T10:00:00", DoneAt: "2020-01-19T10:00:00", Status: true},
-		{Id: 5, EmployeeId: 3, EquipmentType: "Keyboard", EquipmentId: 2, CreatedAt: "2021-01-19T10:00:00", DoneAt: "2021-01-19T10:00:00", Status: true},
-		{Id: 6, EmployeeId: 4, EquipmentType: "Mouse", EquipmentId: 1, CreatedAt: "2021-01-19T10:00:00", DoneAt: "2021-01-19T10:00:00", Status: true},
+		{Id: 1, EmployeeId: 1, EquipmentType: "Laptop", EquipmentId: 1, CreatedAt: time.Now(), DoneAt: time.Now(), Status: Do},
+		{Id: 2, EmployeeId: 1, EquipmentType: "Keyboard", EquipmentId: 1, CreatedAt: time.Now(), DoneAt: time.Now(), Status: InProgress},
+		{Id: 3, EmployeeId: 1, EquipmentType: "Keyboard", EquipmentId: 1, CreatedAt: time.Now(), DoneAt: time.Now(), Status: Done},
+		{Id: 4, EmployeeId: 2, EquipmentType: "Laptop", EquipmentId: 2, CreatedAt: time.Now(), DoneAt: time.Now(), Status: Cancelled},
+		{Id: 5, EmployeeId: 3, EquipmentType: "Keyboard", EquipmentId: 2, CreatedAt: time.Now(), DoneAt: time.Now(), Status: Done},
+		{Id: 6, EmployeeId: 4, EquipmentType: "Mouse", EquipmentId: 1, CreatedAt: time.Now(), DoneAt: time.Now(), Status: Do},
 	},
 }
